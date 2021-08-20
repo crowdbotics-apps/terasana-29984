875b9af6ea2e35d158201ebd99c3ff0ebbe5175c
Crowdbotics <team@crowdbotics.com>
2021-08-20 18:05:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "terasana_29984Identifier",
+  url: "https://terasana_29984Identifier.botics.co",
+  api: "https://terasana_29984Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
